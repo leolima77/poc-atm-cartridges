c8e79858a39871609eec0119fa2f7f535c44c238
Leonardo Lima <leonardo.lima@mbww.com>
2023-12-20 23:15:31 -0300

    first

---
+++ src/Combination.cs
@@ -0,0 +1,33 @@
+﻿namespace AtmCartridges
+{
+    public class Combination
+    {
+        public static List<List<int>> Get(int amount, int tens, int fifties, int hundreds)
+        {
+            List<List<int>> combinations = new List<List<int>>();
+
+            if ((amount % 100 == 0) && (amount / 100 <= hundreds))
+            {
+                combinations.Add(new List<int> { amount / 100, 0, 0 });
+            }
+
+            for (int hu = 0; hu <= Math.Min(hundreds, amount / 100); hu++)
+            {
+                int remainingAmount = amount - hu * 100;
+                for (int fi = 0; fi <= Math.Min(fifties, (remainingAmount / 50)); fi++)
+                {
+                    remainingAmount = amount - hu * 100 - fi * 50;
+                    for (int te = 0; te <= Math.Min(tens, remainingAmount / 10); te++)
+                    {
+                        if (remainingAmount == (te * 10))
+                        {
+                            combinations.Add(new List<int> { hu, fi, te });
+                        }
+                    }
+                }
+            }
+
+            return combinations;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AtmCartridges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] amounts = { 30, 50, 60, 80, 140, 230, 370, 610, 980 };
+            int tens = 10, fifties = 5, hundreds = 2;
+
+            foreach (int amount in amounts)
+            {
+                List<List<int>> combinations = Combination.Get(amount, tens, fifties, hundreds);
+                Console.WriteLine($"Amount: {amount}");
+
+                foreach (List<int> combination in combinations)
+                {
+                    Console.WriteLine($"\t- {combination[0]} x 100 EUR | {combination[1]} x 50 EUR | {combination[2]} x 10 EUR");
+                }
+                Console.WriteLine("---");
+            }
+        }
+    }
+}
